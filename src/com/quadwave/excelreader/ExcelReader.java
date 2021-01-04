1da3785d839823f938cb6ea4fb8fc6e4a608a199
giridhara.mg <giridhar.mg@testyantra.com>
2021-01-04 14:27:51 +0530

    Completed excel-upload task

--- src/com/quadwave/excelreader/ExcelReader.java
+++ src/com/quadwave/excelreader/ExcelReader.java
@@ -1,10 +1,10 @@
 package com.quadwave.excelreader;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
@@ -13,70 +13,88 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.quadwave.nbt.common.axpflow.service.Uploader;
 import com.quadwave.nbt.common.execution.entity.WorkflowInstance;
 import com.quadwave.nbt.common.execution.entity.WorkflowNode;
 import com.quadwave.nbt.common.execution.entity.WorkflowVariable;
 import com.quadwave.nbt.common.execution.java.JavaNodeHandler;
+import com.quadwave.nbt.common.service.dms.vo.BatchOutput;
 
 public class ExcelReader extends JavaNodeHandler {
 
 	@Override
 	public void execute(WorkflowInstance workflowInstance, WorkflowNode workflowNode) {
-		String fileName = "Sample_Input_File.xlsx";
-		try(FileInputStream fis = new FileInputStream(fileName)) {
-
-			// Create Workbook instance for xlsx/xls file input stream
-			Workbook workbook = null;
-			if (fileName.toLowerCase().endsWith("xlsx")) {
-				workbook = new XSSFWorkbook(fis);
-			} else if (fileName.toLowerCase().endsWith("xls")) {
-				workbook = new HSSFWorkbook(fis);
-			}
-
-			// Get the number of sheets in the xlsx file
-			int numberOfSheets = workbook.getNumberOfSheets();
-
-			// loop through each of the sheets
-			for (int i = 0; i < numberOfSheets; i++) {
-
-				// Get the nth sheet from the workbook
-				Sheet sheet = workbook.getSheetAt(i);
-
-				// every sheet has rows, iterate over them
-				Iterator<Row> rowIterator = sheet.iterator();
-				if (rowIterator.hasNext()) {
-					String name = "";
-
-					// Get the row object
-					Row row = rowIterator.next();
-
-					// Every row has columns, get the column iterator and iterate over them
-					Iterator<Cell> cellIterator = row.cellIterator();
-					
-					List<WorkflowVariable> requestVariables = new ArrayList<WorkflowVariable>();
-					while (cellIterator.hasNext()) {
-						// Get the Cell object
-						Cell cell = cellIterator.next();
-
-						// check the cell type and process accordingly
-						switch (cell.getCellType()) {
-						case Cell.CELL_TYPE_STRING:
-							name = cell.getStringCellValue().trim();
-							WorkflowVariable variable = new WorkflowVariable();
-							variable.setName(name);
-							requestVariables.add(variable);
-							break;
-						}
-						workflowInstance.setRequestVariables(requestVariables);
-					} // end of cell iterator
-				} // end of rows iterator
-
-			} // end of sheets for loop
-
+		String dmsPath = "/Temp_" + System.currentTimeMillis();
+		Uploader doUploader = executionContext.getDocumentService().newUploader(workflowInstance.getOrganizationId());
+		
+		WorkflowVariable variable = workflowInstance.getVariableByName("fileupload");
+		System.out.println(variable.getValue());
+		ObjectMapper mapper = new ObjectMapper();
+		String value = variable.getValue();
+		VariableDTO dto = null;
+		try {
+			dto = mapper.readValue(value.substring(1, value.length() - 1), VariableDTO.class);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		if (dto != null) {
+			String fileName = dto.getLocalPath();
+			File file = new File(fileName);
+			doUploader.uploadFiles(dmsPath, Arrays.asList(file), "FilesystemDMS");
+			try (FileInputStream fis = new FileInputStream(fileName)) {
 
+				// Create Workbook instance for xlsx/xls file input stream
+				Workbook workbook = null;
+				if (fileName.toLowerCase().endsWith("xlsx")) {
+					workbook = new XSSFWorkbook(fis);
+				} else if (fileName.toLowerCase().endsWith("xls")) {
+					workbook = new HSSFWorkbook(fis);
+				}
+
+				// Get the number of sheets in the xlsx file
+				int numberOfSheets = workbook.getNumberOfSheets();
+
+				// loop through each of the sheets
+				for (int i = 0; i < numberOfSheets; i++) {
+
+					// Get the nth sheet from the workbook
+					Sheet sheet = workbook.getSheetAt(i);
+
+					// every sheet has rows, iterate over them
+					Iterator<Row> rowIterator = sheet.iterator();
+					if (rowIterator.hasNext()) {
+						String name = "";
+
+						// Get the row object
+						Row row = rowIterator.next();
+
+						// Every row has columns, get the column iterator and iterate over them
+						Iterator<Cell> cellIterator = row.cellIterator();
+
+						while (cellIterator.hasNext()) {
+							// Get the Cell object
+							Cell cell = cellIterator.next();
+
+							// check the cell type and process accordingly
+							switch (cell.getCellType()) {
+							case Cell.CELL_TYPE_STRING:
+								name += cell.getStringCellValue().trim() + ",";
+								break;
+							}
+							
+							WorkflowVariable header_var = workflowInstance.getVariableByName("header_var");
+							header_var.setValue(name);
+							System.out.println(header_var.getValue());
+						} // end of cell iterator
+					} // end of rows iterator
+
+				} // end of sheets for loop
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
---
+++ src/com/quadwave/excelreader/VariableDTO.java
@@ -0,0 +1,41 @@
+package com.quadwave.excelreader;
+
+public class VariableDTO {
+	private String name;
+	private String url;
+	private long size;
+	private String localPath;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public long getSize() {
+		return size;
+	}
+
+	public void setSize(long size) {
+		this.size = size;
+	}
+
+	public String getLocalPath() {
+		return localPath;
+	}
+
+	public void setLocalPath(String localPath) {
+		this.localPath = localPath;
+	}
+
+}
